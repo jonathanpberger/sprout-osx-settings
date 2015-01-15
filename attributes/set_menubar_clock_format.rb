418478ba60ba182152c1d26f1f513b6e89931bd1
Jonathan Berger <jpb@jonathans-air.home>
2015-01-15 13:45:19 -0500

    Update menubar clock format.

--- attributes/set_menubar_clock_format.rb
+++ attributes/set_menubar_clock_format.rb
@@ -1,2 +1,2 @@
 #see http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
-node.default['sprout']['settings']['clock_format'] = 'EEE MMM d  h:mm:ss a'
+node.default['sprout']['settings']['clock_format'] = 'EEE d MMM h:mm a'
